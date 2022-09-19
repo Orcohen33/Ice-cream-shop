631264b29506bcb9b1cb7231458d9d12ea939a75
Or Cohen <92351152+Orcohen33@users.noreply.github.com>
2022-09-19 15:33:06 +0200

    Add files via upload

---
+++ IceCreamShop/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IceCreamShop.MongoDB.PresentationLayer;
+using IceCreamShop.MySQL.PresentationLayer;
+
+Console.WriteLine("[Menu] \n" +
+                  "1. MySQL \n" +
+                  "2. MongoDB \n" +
+                  "3. Exit \n");
+string userInput = Console.ReadLine() ?? "";
+
+do
+{
+    switch (userInput)
+    {
+        case "1":
+            new MySQLMain();
+            break;
+        case "2":
+            new MongoDBMain();
+            break;
+        default:
+            Console.WriteLine("Wrong input, try again");
+            break;
+    }
+} while (userInput != "");
